b91329002f2913b0b8aad8bf05648c92beafa86f
sidhu <venkatasidhartha@gmail.com>
2023-09-29 23:50:29 +0530

    started building Icon

--- App.js
+++ App.js
@@ -7,14 +7,20 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
 import MessagesScreen from "./app/components/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <MessagesScreen />
-            
-        </View>
-    );
+      <Screen>
+        <Icon 
+        name="email"
+        size={50}
+        backgroundColor='red'
+        iconColor="white"
+        />
+      </Screen>
+    )
 }
 
 const styles = StyleSheet.create({
